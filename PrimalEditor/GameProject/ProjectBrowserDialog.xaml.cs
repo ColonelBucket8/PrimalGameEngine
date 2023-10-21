e6c54ffc7d28277ad4a690106a2f2bc82f6a5f12
ColonelBucket8 <freedomterran89@gmail.com>
2023-10-21 22:51:01 +0800

    FEATURE: Create buttons

--- PrimalEditor/GameProject/ProjectBrowserDialog.xaml.cs
+++ PrimalEditor/GameProject/ProjectBrowserDialog.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PrimalEditor.GameProject
 {
@@ -22,6 +10,28 @@ namespace PrimalEditor.GameProject
         public ProjectBrowserDialog()
         {
             InitializeComponent();
+        }
+
+        private void OnToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == openProjectButton)
+            {
+                if (createProjectButton.IsChecked == true)
+                {
+                    createProjectButton.IsChecked = false;
+                    browserContent.Margin = new Thickness(0);
+                }
+                openProjectButton.IsChecked = true;
+            }
+            else
+            {
+                if (openProjectButton.IsChecked == true)
+                {
+                    openProjectButton.IsChecked = false;
+                    browserContent.Margin = new Thickness(-800, 0, 0, 0);
+                }
+                createProjectButton.IsChecked = true;
+            }
         }
     }
 }
