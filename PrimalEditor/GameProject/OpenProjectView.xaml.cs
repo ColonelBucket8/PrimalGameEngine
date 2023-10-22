81e6d5944e07089375275e109bdbcce31ab1dd12
ColonelBucket8 <freedomterran89@gmail.com>
2023-10-22 17:49:55 +0800

    FIX: Main window closed after opening project

--- PrimalEditor/GameProject/OpenProjectView.xaml.cs
+++ PrimalEditor/GameProject/OpenProjectView.xaml.cs
@@ -33,6 +33,7 @@ namespace PrimalEditor.GameProject
             if (project != null)
             {
                 dialogResult = true;
+                win.DataContext = project;
             }
             win.DialogResult = dialogResult;
             win.Close();
