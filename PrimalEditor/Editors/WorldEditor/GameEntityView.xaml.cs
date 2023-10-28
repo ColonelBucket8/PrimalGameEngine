079d7cbebd2066f965908d03a380c86ee4bf3264
ColonelBucket8 <freedomterran89@gmail.com>
2023-10-28 19:52:39 +0800

    FEATURE: Display a selected game entity component

---
+++ PrimalEditor/Editors/WorldEditor/GameEntityView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace PrimalEditor.Editors
+{
+    /// <summary>
+    /// Interaction logic for GameEntityView.xaml
+    /// </summary>
+    public partial class GameEntityView : UserControl
+    {
+        public static GameEntityView Instance { get; private set; }
+        public GameEntityView()
+        {
+            InitializeComponent();
+            DataContext = null;
+            Instance = this;
+        }
+    }
+}
--- PrimalEditor/Editors/WorldEditor/ProjectLayoutView.xaml.cs
+++ PrimalEditor/Editors/WorldEditor/ProjectLayoutView.xaml.cs
@@ -20,5 +20,11 @@ namespace PrimalEditor.Editors
             Scene vm = btn.DataContext as Scene;
             vm.AddGameEntityCommand.Execute(new GameEntity(vm) { Name = "Empty Game Entity" });
         }
+
+        private void OnGameEntities_ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var entity = (sender as ListBox).SelectedItems[0];
+            GameEntityView.Instance.DataContext = entity;
+        }
     }
 }
