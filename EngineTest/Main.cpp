75909885dc6c249ed7425b23d34dab44b14422d2
ColonelBucket8 <freedomterran89@gmail.com>
2023-12-20 14:32:21 +0800

    FEATURE: Add memory leak checker in debug mode

--- EngineTest/Main.cpp
+++ EngineTest/Main.cpp
@@ -1,18 +1,19 @@
 #pragma once
-#include "TestEntityComponents.h"
 
 #define TEST_ENTITY_COMPONENTS 1
 
 #if TEST_ENTITY_COMPONENTS
-
-
+#include "TestEntityComponents.h"
 #else
 #error One of the tests need to be enabled
-
 #endif
 
 int main() {
 
+#if _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+	new float[4];
 	engine_test test{};
 
 	if (test.initialize()) {
